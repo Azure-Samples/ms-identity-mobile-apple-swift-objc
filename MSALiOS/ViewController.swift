5ca330b5c62a47fea82511ab99f369d0fd1f0def
Brandon Werner <brandwe@microsoft.com>
2017-04-08 23:01:15 -0700

    First commit

---
+++ MSALiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSALiOS
+//
+//  Created by Brandon Werner on 4/8/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
