d70e93a00dcedd85adedef3228d233b2f79d2161
Brandon Werner <brandwe@microsoft.com>
2017-04-11 17:45:43 -0700

    Working return to app - need to work on UI for updating app after release.

--- MSALiOS/AppDelegate.swift
+++ MSALiOS/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MSAL
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +41,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        print("Received callback!")
+        
+        MSALPublicClientApplication.handleMSALResponse(url)
+        
+        
+        return true
     }
 
 
--- MSALiOS/ViewController.swift
+++ MSALiOS/ViewController.swift
@@ -7,9 +7,42 @@
 //
 
 import UIKit
+import MSAL
 
-class ViewController: UIViewController {
+let kIssuer = "https://login.microsoftonline.com/brandwedir.onmicrosoft.com/v2.0"
+let kClientID = "2a814505-ab4a-41f7-bd09-3fc614ac077c"
+let kRedirectURI = "msal://com.xerners/"
+let kLogoutURI = "https://login.microsoftonline.com/common/oauth2/v2.0/logout"
+let kScopes: [String] = ["https://graph.microsoft.com/mail.read"]
+let kAuthority = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
 
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBAction func authorizationButton(_ sender: UIButton) {
+        
+        
+        if let application = try? MSALPublicClientApplication.init(clientId: kClientID, authority: kAuthority) {
+
+        application.acquireToken(forScopes: kScopes) { (result, error) in
+            if result != nil {
+                self.loggingText.text = result?.accessToken;
+            } else {
+                print("Error received");
+            }
+        }
+    }
+        
+        else {
+            self.loggingText.text = "unable to create application."
+        }
+}
+
+
+    @IBAction func signoutButton(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var loggingText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +52,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+        
 
 
 }
