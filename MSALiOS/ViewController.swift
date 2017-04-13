777439561466bac57d43a67efe6c83b027d7854f
Brandon Werner <brandwe@microsoft.com>
2017-04-12 17:05:35 -0700

    Updated for cleaner code in Graph API call.

--- MSALiOS/ViewController.swift
+++ MSALiOS/ViewController.swift
@@ -13,36 +13,72 @@ let kIssuer = "https://login.microsoftonline.com/brandwedir.onmicrosoft.com/v2.0
 let kClientID = "2a814505-ab4a-41f7-bd09-3fc614ac077c"
 let kRedirectURI = "msal://com.xerners/"
 let kLogoutURI = "https://login.microsoftonline.com/common/oauth2/v2.0/logout"
-let kScopes: [String] = ["https://graph.microsoft.com/mail.read"]
+let kGraphURI = "https://graph.microsoft.com/v1.0/me/"
+let kScopes: [String] = ["https://graph.microsoft.com/user.read"]
 let kAuthority = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
 
-class ViewController: UIViewController, UITextFieldDelegate {
 
-    @IBAction func authorizationButton(_ sender: UIButton) {
+class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate {
+    
+var msalResult =  MSALResult.init()
+    
+@IBOutlet weak var loggingText: UITextView!
+@IBOutlet weak var signoutButton: UIButton!
+@IBOutlet weak var callGraphApiButton: UIButton!
+
+@IBAction func authorizationButton(_ sender: UIButton) {
         
         
         if let application = try? MSALPublicClientApplication.init(clientId: kClientID, authority: kAuthority) {
 
         application.acquireToken(forScopes: kScopes) { (result, error) in
+            DispatchQueue.main.async {
             if result != nil {
-                self.loggingText.text = result?.accessToken;
+                self.msalResult = result!
+                self.loggingText.text = "Access token is \(self.msalResult.accessToken!)"
+                self.signoutButton.isEnabled = true;
+                self.callGraphApiButton.isEnabled = true;
+                
+                
             } else {
-                print("Error received");
+                self.loggingText.text = "Could not create Public Client instance."
             }
         }
+            }
     }
         
         else {
-            self.loggingText.text = "unable to create application."
+            self.loggingText.text = "Unable to create application."
         }
 }
 
+@IBAction func callGraphApi(_ sender: UIButton) {
+    
+    let sessionConfig = URLSessionConfiguration.default
+    let url = URL(string: kGraphURI)
+    var request = URLRequest(url: url!)
+    request.setValue("Bearer \(msalResult.accessToken!)", forHTTPHeaderField: "Authorization")
+    let urlSession = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: OperationQueue.main)
+    
+    urlSession.dataTask(with: request) { data, response, error in
+        
+        
+            
+        let result = try JSONSerialization.jsonObject(with: responseData, options: [])
+        DispatchQueue.main.async {
+            if result != nil {
+                
+                self.loggingText.text(result?.description)
 
-    @IBAction func signoutButton(_ sender: UIButton) {
+            }
+        }.resume()
+    }
+}
+
+@IBAction func signoutButton(_ sender: UIButton) {
     }
     
-    @IBOutlet weak var loggingText: UITextView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -51,6 +87,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        if self.msalResult.accessToken == nil {
+            
+            signoutButton.isEnabled = false;
+            callGraphApiButton.isEnabled = false;
+            
+            
+        }
     }
     
         
